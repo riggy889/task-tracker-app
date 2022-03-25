3bd76bce77d37d4a5b74993271e2a935abcc0db9
riggy889 <ojpw635@gmail.com>
2022-03-25 16:29:31 +0000

    First commit

---
+++ src/components/Button-Comp.vue
@@ -0,0 +1,24 @@
+<template>
+    <button @click="onClick()" class="btn" :style="{ background: color }">
+        {{text}}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button-Comp',
+    props: {
+        text: String,
+        color: String,
+    },
+    methods:{
+        onClick(){
+            console.log('Click')
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header-Comp.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>   
+        <h1>{{title}}</h1>
+        <Button text="This is a button" color="green" />
+    </header>
+</template>
+
+<script>
+import Button from './Button-Comp.vue'
+
+export default {
+    name: 'Header-Comp',
+    props: {
+        title: String,
+    },
+    components: {
+        Button,
+    }
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
